bdfa22de9b611e96d76f1c631ced56eacefcb4df
gastonpereyra <rgpxen@gmail.com>
2019-07-04 18:14:55 -0300

    Try to change Templates String to Object

--- src/api-gateway-resource-template.js
+++ src/api-gateway-resource-template.js
@@ -5,6 +5,22 @@ module.exports = ({
 	parentResourceName,
 	resourcePathPart
 }) => `
+${resourceName}:
+  Type: AWS::ApiGateway::Resource
+  Properties:
+    ParentId: ${parentResourceName ? `!Ref ${parentResourceName}` : `
+      Fn::ImportValue:
+        !Sub '\${ApiGatewayStackName}-ApiGatewayRootResourceId'`}
+    PathPart: '${resourcePathPart}'
+    RestApiId:
+      Fn::ImportValue:
+        !Sub '\${ApiGatewayStackName}-ApiGatewayId'
+`;
+
+
+/**
+ *
+  `
     ${resourceName}:
       Type: AWS::ApiGateway::Resource
       Properties:
@@ -16,3 +32,29 @@ module.exports = ({
           Fn::ImportValue:
             !Sub '\${ApiGatewayStackName}-ApiGatewayId'
   `;
+
+
+const resourceTemplate = {};
+
+	const importValue = {
+		'Fn::ImportValue': '!Sub \'${ApiGatewayStackName}-ApiGatewayRootResourceId\''
+	};
+
+	const ref = `!Ref ${parentResourceName}`;
+
+	const ParentId = parentResourceName ? ref : importValue;
+
+	resourceTemplate[resourceName] = {
+		Type: 'AWS::ApiGateway::Resource',
+		Properties: {
+			ParentId,
+			PathPart: resourcePathPart,
+			RestApiId: {
+				'Fn::ImportValue': '!Sub \'${ApiGatewayStackName}-ApiGatewayId\''
+			}
+		}
+	};
+
+	return resourceTemplate;
+
+ */
